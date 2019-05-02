0f81d2e844223a85cd93960cfbc1e667ea4147ad
Ente <ducksource@duckpond.ch>
2019-05-03 00:07:57 +0200

    Set default target for all dependencies

--- default.nix
+++ default.nix
@@ -1,9 +1,37 @@
 with import <nixpkgs> {};
+let
+  rust = (import ./rust.nix);
+  extraRustcOpts = [ "--target x86_64-unknown-linux-musl" ];
+in
 ((import ./Cargo.nix).kamikaze {}).override {
-  rust = (import ./shell.nix).rust;
+  rust = rust;
   crateOverrides = defaultCrateOverrides // {
     kamikaze = attrs: {
-      extraRustcOpts = [ "--target x86_64-unknown-linux-musl" ];
+      extraRustcOpts = extraRustcOpts;
+    };
+    errno = attrs: {
+      extraRustcOpts = extraRustcOpts;
+    };
+    errno-dragonfly = attrs: {
+      extraRustcOpts = extraRustcOpts;
+    };
+    exec = attrs: {
+      extraRustcOpts = extraRustcOpts;
+    };
+    gcc = attrs: {
+      extraRustcOpts = extraRustcOpts;
+    };
+    libc = attrs: {
+      extraRustcOpts = extraRustcOpts;
+    };
+    winapi = attrs: {
+      extraRustcOpts = extraRustcOpts;
+    };
+    winapi-i686-pc-windows-gnu = attrs: {
+      extraRustcOpts = extraRustcOpts;
+    };
+    winapi-x86_64-pc-windows-gnu = attrs: {
+      extraRustcOpts = extraRustcOpts;
     };
   };
 }
